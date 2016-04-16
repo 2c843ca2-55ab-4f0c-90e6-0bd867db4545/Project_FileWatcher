63a16b52245c50a5a8380748a3f4bba2969bd53d
2c843ca2-55ab-4f0c-90e6-0bd867db4545 <chenakeshava1@gmail.com>
2016-04-16 21:32:50 +0530

    Initial Commit

---
+++ FileWatcher/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Permissions;
+using System.IO;
+using System.Collections;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public static Hashtable dt = new Hashtable();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        watcher.Path = "E:\\Temp\\";
+        watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+           | NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.CreationTime;
+        watcher.Changed += new FileSystemEventHandler(OnChanged);
+        watcher.EnableRaisingEvents = true;
+        watcher.Filter = "*.xml";
+
+    }
+
+    private void OnChanged(object source, FileSystemEventArgs e)
+    {
+        //FileSystemWatcher et = (FileSystemWatcher)source;
+        // Specify what is done when a file is changed, created, or deleted.
+        //Console.WriteLine("File: " + e.Name + " " + e.ChangeType);
+        string date = File.GetLastWriteTime(e.FullPath).ToString();
+        if (!dt.ContainsKey(e.Name))
+            dt.Add(e.Name, date);
+        else
+            dt[e.Name] = date;
+
+        Application["FileDet"] = dt;
+    }
+}
---
+++ FileWatcher/Default2.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Data;
+using System.IO;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string filename = "ListProduct";
+        
+        if (Application["FileDet"] != null)
+        {
+            Hashtable dt = (Hashtable)Application["FileDet"];
+            if (dt.ContainsKey(filename+".xml"))
+            {
+                Response.Write(File.ReadAllText("E:\\Temp\\ListProduct.xml"));
+                Response.End();
+            }
+        }
+    }
+}
